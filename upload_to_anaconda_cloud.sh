efe4108375b07d43a0820c433228aadadd83691b
Helge <helge.dzierzon@brockmann-consult.de>
2018-10-12 15:26:14 +0200

    upload is now looping through distributions

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -7,16 +7,19 @@ echo "event:   $TRAVIS_EVENT_TYPE"
 echo "package: $1"
 echo
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    if [ "$TRAVIS_EVENT_TYPE" = "api" -o "$TRAVIS_EVENT_TYPE" = "push" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    if [ "${TRAVIS_EVENT_TYPE}" = "api" -o "${TRAVIS_EVENT_TYPE}" = "push" ]; then
         CONDA_PACKAGE=$(conda build --old-build-string -c conda-forge -c defaults recipes/$1 --output);
-        echo "conda package $CONDA_PACKAGE";
-        anaconda -v -t $ANACONDA_TOKEN upload "$CONDA_PACKAGE" -u bc-dev --force;
+        for p in ${CONDA_PACKAGE}
+        do
+            echo "conda package ${p}";
+            anaconda -v -t ${ANACONDA_TOKEN} upload "${p}" -u bc-dev --force;
+         done
     else
         echo "NOT uploading to Anaconda Cloud, because this is the wrong event type";
-        echo "event $TRAVIS_EVENT_TYPE"
+        echo "event ${TRAVIS_EVENT_TYPE}"
     fi
 else
     echo "NOT uploading to Anaconda Cloud, because this is the wrong branch";
-    echo "branch $TRAVIS_BRANCH";
+    echo "branch ${TRAVIS_BRANCH}";
 fi
