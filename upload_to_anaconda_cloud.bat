ee4f29c585a00a1ea7ce5f8ebbfe8bb2ca31f377
Helge <helge.dzierzon@brockmann-consult.de>
2018-10-22 16:39:11 +0200

    Removed a spello

--- upload_to_anaconda_cloud.bat
+++ upload_to_anaconda_cloud.bat
@@ -4,7 +4,7 @@ REM Build conda build command
 
 SET BUILD=conda build --old-build-string -c conda-forge -c anaconda recipes\gridtools --output
 
-REM Run bild and loop through results
+REM Run build and loop through results
 
 FOR /F  "tokens=*" %%b in ('%BUILD%') DO (
     anaconda -v -t %1 upload %%b --force
